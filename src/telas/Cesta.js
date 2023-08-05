c7e018ed672a2d9dfe948e3a1788b81ea7936627
Iraponan Marinho <iraponan.aires@gmail.com>
2023-08-05 16:57:55 -0300

    Criando a Cesta

--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -1,5 +1,30 @@
-import { Text } from 'react-native';
+import { Dimensions, Image, StyleSheet, Text } from 'react-native';
+
+import topo from '../../assets/topo.png'
+
+const width = Dimensions.get('screen').width;
 
 export default function Cesta() {
-    return <Text>Cesta</Text>
+    return <>
+        <Image source={topo} style={estilos.topo}/>
+        <Text style={estilos.titulo}>Detalhes da Cesta</Text>
+    </> 
 }
+
+const estilos = StyleSheet.create({
+    topo: {
+        width: "100%",
+        height: 578 / 768 * width
+    },
+
+    titulo: {
+        width: "100%",
+        position: "absolute",
+        textAlign: "center",
+        fontSize: 16,
+        lineHeight: 26,
+        color: "white",
+        fontWeight: "bold",
+        padding: 16
+    }
+})
