8a5351da5d4674e38f6f58939bac472ae417bb28
Iraponan Marinho <iraponan.aires@gmail.com>
2023-08-05 23:47:05 -0300

    Implementando SplashScreen

--- App.js
+++ App.js
@@ -1,22 +1,57 @@
-import { StatusBar, SafeAreaView, View } from 'react-native';
+import { useCallback, useEffect, useState } from 'react';
+import { StatusBar, SafeAreaView } from 'react-native';
 
-import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
+import { Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
+import * as Font from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 
 import Cesta from './src/telas/Cesta';
 import mock from './src/mocks/cesta';
 
-export default function App() {
-  const [fonteCarregada] = useFonts({
-    "MontserratRegular": Montserrat_400Regular,
-    "MontserratBold": Montserrat_700Bold
-  });
+SplashScreen.preventAutoHideAsync();
 
-  if (!fonteCarregada) {
-    return <View />
-  };
+export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync({
+          "MontserratRegular": Montserrat_400Regular,
+          "MontserratBold": Montserrat_700Bold
+        });
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        // await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
 
   return (
-    <SafeAreaView>
+    <SafeAreaView onLayout={onLayoutRootView}>
       <StatusBar />
       <Cesta {...mock}/> 
     </SafeAreaView> 
